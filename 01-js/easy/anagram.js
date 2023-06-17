32768970a91a891995d5bdbdf051a65edc895d19
Prem DMA <31176038+PremDMA@users.noreply.github.com>
2023-06-17 16:07:32 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,31 @@
 */
 
 function isAnagram(str1, str2) {
+  var charMapStr1 = getCharMap(str1);
+  var charMapStr2 = getCharMap(str2);
 
+  var mapKeys1 = Object.keys(charMapStr1);
+  var mapKeys2 = Object.keys(charMapStr2);
+
+  if(mapKeys1.length != mapKeys2.length) {
+    return false;
+  }
+
+  for( let i=0 ; i<mapKeys1.length ;i++) {
+    if( charMapStr1[mapKeys1[i]] != charMapStr2[mapKeys1[i]]) {
+      return false;
+    }
+    
+  }
+  return true;
+}
+
+function getCharMap(str) {
+  var charMap = {};
+  for( let i =0 ; i< str.length ; i++) {
+    charMap[str.charAt(i)] = charMap[str.charAt(i)] ? charMap[str.charAt(i)] +1 : 1;
+  }
+  return charMap;
 }
 
 module.exports = isAnagram;
